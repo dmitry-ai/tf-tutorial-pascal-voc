e101968ecb9b37fa2f912ad34c3763943f280c58
Dmitry Fedyuk <dfediuk@gmail.com>
2019-05-13 05:48:05 +0600

    no message

--- training.sh
+++ training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 objectDetection='/w/tools/tensorflow/models/research/object_detection/'
 python ${objectDetection}model_main.py \
-	--pipeline_config_path=faster_rcnn_resnet101_voc07.config \
+	--pipeline_config_path=/w/tutorials/tensorflow/pascal-voc/faster_rcnn_resnet101_voc07.config \
 	--model_dir=/w/tutorials/tensorflow/pascal-voc/model \
 	--num_train_steps=100 \
 	--sample_1_of_n_eval_examples=1 \
