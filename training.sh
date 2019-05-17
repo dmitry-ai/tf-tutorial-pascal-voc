cf541f4371af61b7b26d37aed783341d05de2e16
Dmitry Fedyuk <dfediuk@gmail.com>
2019-05-17 06:06:54 +0600

    no message

--- training.sh
+++ training.sh
@@ -2,7 +2,7 @@
 objectDetection='/w/tools/tensorflow/models/research/object_detection/'
 python ${objectDetection}model_main.py \
 	--pipeline_config_path=faster_rcnn_resnet101_voc07.pbtxt \
-	--model_dir=../run/10000 \
+	--model_dir=../run/2019-05-17 \
 	--num_train_steps=10000 \
 	--sample_1_of_n_eval_examples=1 \
 	--alsologtostderr
